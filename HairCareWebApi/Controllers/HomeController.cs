d4b847712630506c2b439b4a37e81d7626c511a1
Carissa Felder <carissa.felder@gmail.com>
2017-03-28 14:38:43 -0400

    Removed registration;added google api; fb api has a bug from microsoft due to fb upgrade.

---
+++ HairCareWebApi/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HairCareWebApi.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [AllowAnonymous]//Don't ask them to log in/ you can see just see it.
+        public ActionResult Index()
+        {
+            var clientId = ConfigurationManager.AppSettings["googleClientId"];
+            var Secret = ConfigurationManager.AppSettings["googleSecret"];
+           //var fbappId = ConfigurationManager.AppSettings["facebookClientId"];
+           // var fbappID = ConfigurtionManager.AppSettings["facebookSecret"];
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
